e51ce2ed1dbf39b88631e6280296b3d7999ce921
amalbert007 <amyalbert007@gmail.com>
2021-05-28 22:01:37 +0530

    First Commit-I

---
+++ DryCleaningApp/src/main/java/com/cg/drycleaningapp/DryCleaningApp.java
@@ -0,0 +1,12 @@
+package com.cg.drycleaningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DryCleaningApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DryCleaningApp.class, args);
+	}
+
+}
